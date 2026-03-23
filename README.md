2ea3d1e6e3f4c955d3d378bdb1e25bc242f0a9aa
Maor Sharon <maor@zappy5.com>
2026-03-23 18:53:28 +0200

    Background sync: Progressive GitHub Sync

--- README.md
+++ README.md
@@ -70,7 +70,7 @@ This site is automatically deployed to Render as a static site serving from the 
 
 ## Generated on
 
-2026-03-23T16:43:55.526Z
+2026-03-23T16:53:13.580Z
 
 ---
 
--- script.js
+++ script.js
@@ -2142,6 +2142,8 @@ window.onload = function() {
 ;
 
 ;
+
+;
 /* ==ZAPPY E-COMMERCE JS START== */
 // E-commerce functionality
 (function() {
@@ -3116,43 +3118,50 @@ function stripHtmlToText(html) {
       var priceHtml = showPrice ? '<div class="price">' + displayPrice + '</div>' + pricePerUnitHtml : '';
       
       var favBtnHtml = '<button type="button" class="card-favorite-btn" data-product-id="' + p.id + '" onclick="event.preventDefault(); event.stopPropagation(); toggleCardFavorite(this, \'' + p.id + '\')" title="שמור למועדפים" aria-pressed="false"><svg class="heart-outline" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20" fill="none"><path d="M14.7917 0.833C12.705 0.833 10.811 2.376 10 4.462C9.189 2.375 7.295 0.833 5.208 0.833C2.337 0.833 0 3.17 0 6.042C0 11.675 8.128 17.767 9.758 18.93L10 19.104L10.243 18.93C11.873 17.767 20 11.674 20 6.042C20 3.17 17.663 0.833 14.792 0.833ZM10 18.078C5.716 14.965 0.833 10.019 0.833 6.042C0.833 3.629 2.796 1.667 5.208 1.667C7.498 1.667 9.583 4.05 9.583 6.667H10.417C10.417 4.05 12.502 1.667 14.792 1.667C17.204 1.667 19.167 3.629 19.167 6.042C19.167 10.019 14.284 14.965 10 18.078Z" fill="currentColor"/></svg><svg class="heart-filled" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path d="M14.7917 0.833C12.705 0.833 10.811 2.376 10 4.462C9.189 2.375 7.295 0.833 5.208 0.833C2.337 0.833 0 3.17 0 6.042C0 11.675 8.128 17.767 9.758 18.93L10 19.104L10.243 18.93C11.873 17.767 20 11.674 20 6.042C20 3.17 17.663 0.833 14.792 0.833Z" fill="#e74c3c"/></svg></button>';
-      var productCardMediaHtml = '<div class="product-card-media">' + imageHtml + tagsHtml + favBtnHtml + '</div>';
+      var productHref = '/product/' + (p.slug || p.id);
+      var productCardMediaHtml = '<div class="product-card-media"><a href="' + productHref + '" class="product-card-image-link">' + imageHtml + '</a>' + tagsHtml + favBtnHtml + '</div>';
 
       if (productLayout === 'compact') {
         cardContent =
-          '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+          '<div class="product-card-inner">' +
             productCardMediaHtml +
-            '<div class="card-content">' +
-              '<h3>' + p.name + '</h3>' +
-              priceHtml +
-            '</div>' +
-          '</a>';
+            '<a href="' + productHref + '" class="product-card-body-link">' +
+              '<div class="card-content">' +
+                '<h3>' + p.name + '</h3>' +
+                priceHtml +
+              '</div>' +
+            '</a>' +
+          '</div>';
       } else if (productLayout === 'detailed') {
         var detailedDesc = stripHtmlToText(p.description || '');
         var actionButton = isCatalogMode
-          ? '<a href="/product/' + (p.slug || p.id) + '" class="add-to-cart view-details-btn">' + localizedViewDetails + '</a>'
+          ? '<a href="' + productHref + '" class="add-to-cart view-details-btn">' + localizedViewDetails + '</a>'
           : '<button class="add-to-cart" onclick="event.stopPropagation(); window.zappyHandleAddToCart(' + JSON.stringify(p).replace(/"/g, '&quot;') + ')">' + localizedAddToCart + '</button>';
         cardContent =
-          '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+          '<div class="product-card-inner">' +
             productCardMediaHtml +
-            '<div class="card-content">' +
-              '<h3>' + p.name + '</h3>' +
-              '<p>' + detailedDesc + '</p>' +
-              priceHtml +
-            '</div>' +
-          '</a>' +
+            '<a href="' + productHref + '" class="product-card-body-link">' +
+              '<div class="card-content">' +
+                '<h3>' + p.name + '</h3>' +
+                '<p>' + detailedDesc + '</p>' +
+                priceHtml +
+              '</div>' +
+            '</a>' +
+          '</div>' +
           actionButton;
       } else {
         var standardDesc = stripHtmlToText(p.description || '');
         cardContent =
-          '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+          '<div class="product-card-inner">' +
             productCardMediaHtml +
-            '<div class="card-content">' +
-              '<h3>' + p.name + '</h3>' +
-              '<p>' + standardDesc + '</p>' +
-              priceHtml +
-            '</div>' +
-          '</a>';
+            '<a href="' + productHref + '" class="product-card-body-link">' +
+              '<div class="card-content">' +
+                '<h3>' + p.name + '</h3>' +
+                '<p>' + standardDesc + '</p>' +
+                priceHtml +
+              '</div>' +
+            '</a>' +
+          '</div>';
       }
       
       return '<div class="product-card ' + productLayout + '" data-product-id="' + p.id + '">' + cardContent + '</div>';
@@ -6824,43 +6833,50 @@ function renderProductGrid(grid, products, t, isFeaturedSection, viewMode) {
     var priceHtml = showPrice ? '<div class="price">' + displayPrice + '</div>' + pricePerUnitHtml : '';
     
     var favBtnHtml = '<button type="button" class="card-favorite-btn" data-product-id="' + p.id + '" onclick="event.preventDefault(); event.stopPropagation(); toggleCardFavorite(this, \'' + p.id + '\')" title="שמור למועדפים" aria-pressed="false"><svg class="heart-outline" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20" fill="none"><path d="M14.7917 0.833C12.705 0.833 10.811 2.376 10 4.462C9.189 2.375 7.295 0.833 5.208 0.833C2.337 0.833 0 3.17 0 6.042C0 11.675 8.128 17.767 9.758 18.93L10 19.104L10.243 18.93C11.873 17.767 20 11.674 20 6.042C20 3.17 17.663 0.833 14.792 0.833ZM10 18.078C5.716 14.965 0.833 10.019 0.833 6.042C0.833 3.629 2.796 1.667 5.208 1.667C7.498 1.667 9.583 4.05 9.583 6.667H10.417C10.417 4.05 12.502 1.667 14.792 1.667C17.204 1.667 19.167 3.629 19.167 6.042C19.167 10.019 14.284 14.965 10 18.078Z" fill="currentColor"/></svg><svg class="heart-filled" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 20 20"><path d="M14.7917 0.833C12.705 0.833 10.811 2.376 10 4.462C9.189 2.375 7.295 0.833 5.208 0.833C2.337 0.833 0 3.17 0 6.042C0 11.675 8.128 17.767 9.758 18.93L10 19.104L10.243 18.93C11.873 17.767 20 11.674 20 6.042C20 3.17 17.663 0.833 14.792 0.833Z" fill="#e74c3c"/></svg></button>';
-    var productCardMediaHtml = '<div class="product-card-media">' + imageHtml + tagsHtml + favBtnHtml + '</div>';
+    var productHref = '/product/' + (p.slug || p.id);
+    var productCardMediaHtml = '<div class="product-card-media"><a href="' + productHref + '" class="product-card-image-link">' + imageHtml + '</a>' + tagsHtml + favBtnHtml + '</div>';
 
     if (layout === 'compact') {
       cardContent =
-        '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+        '<div class="product-card-inner">' +
           productCardMediaHtml +
-          '<div class="card-content">' +
-            '<h3>' + p.name + '</h3>' +
-            priceHtml +
-          '</div>' +
-        '</a>';
+          '<a href="' + productHref + '" class="product-card-body-link">' +
+            '<div class="card-content">' +
+              '<h3>' + p.name + '</h3>' +
+              priceHtml +
+            '</div>' +
+          '</a>' +
+        '</div>';
     } else if (layout === 'detailed') {
       var detailedDesc = stripHtmlToText(p.description || '');
       var actionButton = isCatalogMode
-        ? '<a href="/product/' + (p.slug || p.id) + '" class="add-to-cart view-details-btn">' + localizedViewDetails + '</a>'
+        ? '<a href="' + productHref + '" class="add-to-cart view-details-btn">' + localizedViewDetails + '</a>'
         : '<button class="add-to-cart" onclick="event.stopPropagation(); window.zappyHandleAddToCart(' + JSON.stringify(p).replace(/"/g, '&quot;') + ')">' + localizedAddToCart + '</button>';
       cardContent =
-        '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+        '<div class="product-card-inner">' +
           productCardMediaHtml +
-          '<div class="card-content">' +
-            '<h3>' + p.name + '</h3>' +
-            '<p>' + detailedDesc + '</p>' +
-            priceHtml +
-          '</div>' +
-        '</a>' +
+          '<a href="' + productHref + '" class="product-card-body-link">' +
+            '<div class="card-content">' +
+              '<h3>' + p.name + '</h3>' +
+              '<p>' + detailedDesc + '</p>' +
+              priceHtml +
+            '</div>' +
+          '</a>' +
+        '</div>' +
         actionButton;
     } else {
       var standardDesc = stripHtmlToText(p.description || '');
       cardContent =
-        '<a href="/product/' + (p.slug || p.id) + '" class="product-card-link">' +
+        '<div class="product-card-inner">' +
           productCardMediaHtml +
-          '<div class="card-content">' +
-            '<h3>' + p.name + '</h3>' +
-            '<p>' + standardDesc + '</p>' +
-            priceHtml +
-          '</div>' +
-        '</a>';
+          '<a href="' + productHref + '" class="product-card-body-link">' +
+            '<div class="card-content">' +
+              '<h3>' + p.name + '</h3>' +
+              '<p>' + standardDesc + '</p>' +
+              priceHtml +
+            '</div>' +
+          '</a>' +
+        '</div>';
     }
     
     return '<div class="product-card ' + layout + '" data-product-id="' + p.id + '">' + cardContent + '</div>';
